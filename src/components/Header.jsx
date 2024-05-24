0c6a55e5c9cb489a2610fe79ddb1a9d129d83fe0
shehab <shehab.mm@gmail.com>
2024-05-24 14:21:06 +0300

    tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header({ name }) {
-  return <div>This is our Header sas {name}</div>;
+  return <div className="text-4xl">This is our Header sas {name}</div>;
 }
 
 export default Header ;
