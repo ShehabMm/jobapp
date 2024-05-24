34e94e9c46761df239fb999a4dadffb6d3b4a6e9
shehab <shehab.mm@gmail.com>
2024-05-24 19:20:52 +0300

    Router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../assets/logo.png'
-
+import { Link } from 'react-router-dom';
 
 function NavBar() {
 
@@ -15,23 +15,23 @@ function NavBar() {
         <div className='flex h-20 items-center justify-between'>
           <div className='flex flex-1 items-center justify-center md:items-stretch md:justify-start'>
          
-            <a className='flex flex-shrink-0 items-center mr-4' href='/'>
+            <Link className='flex flex-shrink-0 items-center mr-4' to='/'>
               <img className='h-10 w-auto' src={logo} alt='React Jobs' />
               <span className='hidden md:block text-white text-2xl font-bold ml-2'>
                 React Jobs
               </span>
-            </a>
+            </Link>
             <div className='md:ml-auto'>
               <div className='flex space-x-2'>
-                <a href='/' className={linkClass}>
+                <Link to='/' className={linkClass}>
                   Home
-                </a>
-                <a href='/jobs' className={linkClass}>
+                </Link>
+                <Link to='/jobs' className={linkClass}>
                   Jobs
-                </a>
-                <a href='/add-job' className={linkClass}>
+                </Link>
+                <Link to='/add-job' className={linkClass}>
                   Add Job
-                </a>
+                </Link>
               </div>
             </div>
           </div>
