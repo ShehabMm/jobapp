a7393eaad0e6e65b7b1d6b810e06b9ebda903aec
shehab <shehab.mm@gmail.com>
2024-05-24 13:01:20 +0300

    header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const name = 'shehab'
+const App = () => {
+  return (
+    <>
+      <Header name = {name}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header({ name }) {
+  return <div>This is our Header {name}</div>;
+}
+
+export default Header;
